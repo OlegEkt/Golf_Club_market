516569fdde8070fb9098ed5a4237999aaa08a05f
OlegEkt <scala37@yandex.ru>
2024-01-28 17:40:19 +0500

    new files

--- djmarketplace/app_shop/forms.py
+++ djmarketplace/app_shop/forms.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User, GoodCart
 from django import forms
 
 class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+class CartAddForm(forms.ModelForm):
+
+    class Meta:
+        model = GoodCart
+        fields = ('good_num', )
